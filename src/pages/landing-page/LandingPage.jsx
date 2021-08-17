b6588531bce0533d9f9da7e7bb32d05676100b53
Zaheer Abbas <nk4456542@gmail.com>
2021-08-18 01:03:24 +0530

    Solve Rebasing and Merging Issues

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Navbar from "../../components/navbar";
 // import Header from "../../components/header";
 import PauseOnHover from "../../components/slider";
-function LandingPage() {
+function LandingPage(props) {
+  const { isSignedIn, signIn } = props;
+
   return (
     <div>
-      <Navbar />
+      <Navbar isSignedIn={isSignedIn} signIn={signIn} />
       {/* <Header /> */}
       {/* <Navbar /> */}
       <PauseOnHover />
