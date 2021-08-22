ed9d7b2918d6bac59531269d44a31dd6450aeb60
Pratyush Kumar <kpratyush12345@gmail.com>
2021-08-22 18:49:52 +0530

    box shadow added

--- src/components/user-profile/UserProfile.jsx
+++ src/components/user-profile/UserProfile.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Avatar } from "@material-ui/core";
+import PersonIcon from "@material-ui/icons/Person";
+import { Link } from "react-router-dom";
 import "./UserProfile.css";
 
 function UserProfile(props) {
@@ -16,15 +18,21 @@ function UserProfile(props) {
 
   return (
     <div className="user-profile">
-      <Avatar onClick={handleAvatarClick} />
-      <p>{userData.userName}</p>
-
-      {/* {isDropdownOpen && (
+      <div className="login-icon" onClick={handleAvatarClick}>
+        <PersonIcon />
+        <p>{userData.userName}</p>
+      </div>
+      {isDropdownOpen && (
         <div className="user-avatar">
-          <p>{userData.userEmail}</p>
-          <p>{userData.userName}</p>
+          <Link to="/orders">
+            <p>Orders</p>
+          </Link>
+          <p>Wishlist</p>
+          <p>Order History</p>
+          <p>Gift Cards</p>
+          <p>Contact Us</p>
         </div>
-      )} */}
+      )}
     </div>
   );
 }
