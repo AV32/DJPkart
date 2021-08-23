71f2ca10177702680032235f49846277f6ae7433
sanctum007 <pushpendrayadav1057@gmail.com>
2021-08-23 16:19:23 +0530

    fixed wishlist bug

--- src/pages/productpage/ProductPage.jsx
+++ src/pages/productpage/ProductPage.jsx
@@ -218,10 +218,11 @@ function ProductPage(props) {
                 <button
                   onClick={() => {
                     addItemToWishlist({
-                      id: props.id,
-                      name: props.name,
-                      price: props.price,
-                      rating: props.rating,
+                      id: id,
+                      name: name,
+                      price: price,
+                      rating: rating,
+                      img: image[0],
                     });
                     setInWishList(true);
                   }}
